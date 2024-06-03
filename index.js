0a2dbfa32118fba7b99d9398cd274c7a64914043
Tijil Agrawal <77985046+Tijil2111@users.noreply.github.com>
2024-06-03 23:34:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+function validateForm() {
+  let email = document.forms["form"]["email"].value;
+  for (let i = 0; i < email.length; i++) {
+    if (!email.includes("@", 0)) {
+      alert("Please enter a valid email");
+      return false;
+    }
+  }
+
+  let name = document.forms["form"]["name"].value;
+  let achievements = document.forms["form"]["achievements"].value;
+  let dob = document.forms["form"]["date"].value;
+  let grade = document.forms["form"]["grade"].value;
+  let checked = document.forms["form"]["radio"].checked;
+
+  alert(
+    "We have successfully received the voyagers data in our databases. It is as follows"
+  );
+  alert("Name: " + name);
+  alert("Email: " + email);
+  alert("Achievements: " + achievements);
+  alert("Date of Birth: " + dob);
+  alert("Grade: " + grade);
+  if (checked === true) {
+    alert("You have accepted to recieve the notifications.");
+  }
+  alert("Thank you. We look forward to meet you on the fleet.");
+  alert(
+    "hello exun team ! this side tijil. just a little thing, the data gets sent to my mail id as i used formsbumit.io. thanksss for visiting :)"
+  );
+}
